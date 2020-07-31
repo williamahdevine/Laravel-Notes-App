9b9a4192c51aa6c295327f663250a7ceaa7f71d7
williamahdevine <64220756+williamahdevine@users.noreply.github.com>
2020-07-31 12:40:13 -0300

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,7 +20,7 @@
         </div>
     
         <div class="field">
-            <label for="email">E-Mail Address</label>
+            <label for="email">Password</label>
             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
             @error('email')
